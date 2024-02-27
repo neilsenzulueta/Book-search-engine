c2cf52aee825b8089e6118681fa90fa467f6c4ed
neilsen zulueta <neilzulueta@gmail.com>
2024-02-27 13:56:54 -0800

    updated typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,19 +1,45 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-    description: String
-  }
+    type Query {
+        me: User
+    }
 
-  type Query {
-    books: [Book]
-  }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(
+            authors: [String!]!,
+            description: String!,
+            title: String!,
+            bookId: String!,
+            image: String!,
+            link: String!
+        ): User
+        removeBook(bookId: String!): User
+    }
 
-  type Mutation {
-    addBook(title: String!, author: String!, description: String!): Book
-  }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: String
+        user: User
+    }
 `;
 
 module.exports = typeDefs;
