e72ac6b75ea73ef39566efa4f6895a2ed8f93399
neilsen zulueta <neilzulueta@gmail.com>
2024-02-27 18:57:35 -0800

    created queries.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -13,7 +13,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -35,10 +35,5 @@ module.exports = {
 
     // send to next endpoint
     next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
